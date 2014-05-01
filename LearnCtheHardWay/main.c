316c975e434969ffcccc09b068a203f3d93d945f
ellmo <ellmunzai@gmail.com>
2014-05-02 01:16:18 +0200

    Initial Commit

---
+++ LearnCtheHardWay/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  LearnCtheHardWay
+//
+//  Created by Jakub Żuchowski on 02.05.2014.
+//  Copyright (c) 2014 Jakub Żuchowski. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
